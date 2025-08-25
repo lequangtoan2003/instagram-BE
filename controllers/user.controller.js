840a88e8e00c635f6d7d5141a202f58c12d238f0
unknown <toan9370@donga.edu.vn>
2025-08-25 15:19:52 +0700

    api-post-and-cmt-data

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import { Post } from "../models/post.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./configs/db.js";
 import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
@@ -23,6 +25,8 @@ app.use(cookieParser());
 
 // Import routes
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
 app.listen(PORT, () => {
   connectDB();
